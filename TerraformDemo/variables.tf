cd61641198a6bd558a36c961fb3b3fd6ec850080
siaogina <90738474+mingwei1744@users.noreply.github.com>
2023-03-06 23:22:42 +0800

    Updated with program flow

--- TerraformDemo/variables.tf
+++ TerraformDemo/variables.tf
@@ -10,41 +10,41 @@ variable "location" {
 variable "naming" {
   description = "Naming convention of objects"
   type        = string
-  default     = "automato"
+  #default     = "automato"
 }
 
 variable "vm-dns" {
   description = "DNS for VM public IP"
   type        = string
-  default     = "dns-xm-test" // CHANGE THIS TO YOUR PREFERRED VM DNS *UNQ
+  #default     = "dns-xm-test" // CHANGE THIS TO YOUR PREFERRED VM DNS *UNQ
 }
 
 variable "admin-user" {
   description = "Admin user of server (root)"
   type        = string
-  default     = "adminuser"
+  #default     = "adminuser"
 }
 
 variable "web-user" {
   description = "Web application user of server"
   type        = string
-  default     = "webadm"
+  #default     = "webadm"
 }
 
 variable "website-dns" {
   description = "Domain name for website public IP"
   type        = string
-  default     = "WWW.YOURPBDOMAIN.COM" // CHANGE THIS TO YOUR PORKBUN DOMAIN NAME *UNQ
+  #default     = "WWW.YOURPBDOMAIN.COM" // CHANGE THIS TO YOUR PORKBUN DOMAIN NAME *UNQ
 }
 
 variable "database-pwd" {
   description = "Access code for database"
   type        = string
-  default     = "dbP@assw0rd"
+  #default     = "dbP@assw0rd"
 }
 
 variable "email" {
   description = "Email for certbot"
   type        = string
-  default     = "YOUREMAIL@EMAIL.COM" // CHANGE TO YOUR EMAIL
+  #default     = "YOUREMAIL@EMAIL.COM" // CHANGE TO YOUR EMAIL
 }
---
+++ configpackage/configclass.py
@@ -0,0 +1,52 @@
+class UserConfig:
+    def __init__(self):
+
+        self.resource_naming = ""
+        self.vm_dns = ""
+        self.vm_rootuser = ""
+        self.vm_webuser = ""
+        self.web_domain_name = ""
+        self.database_key = ""
+        self.email = ""
+
+    def get_resource_naming(self):
+        return self.resource_naming
+
+    def set_resource_naming(self, idata):
+        self.resource_naming = idata
+
+    def get_vm_dns(self):
+        return self.vm_dns
+
+    def set_vm_dns(self, idata):
+        self.vm_dns = idata
+
+    def get_rootuser(self):
+        return self.vm_rootuser
+
+    def set_rootuser(self, idata):
+        self.vm_rootuser = idata
+
+    def get_webuser(self):
+        return self.vm_webuser
+
+    def set_webuser(self, idata):
+        self.vm_webuser = idata
+
+    def get_web_domain_name(self):
+        return self.web_domain_name
+
+    def set_web_domain_name(self, idata):
+        self.web_domain_name = idata
+
+    def get_database_key(self):
+        return self.database_key
+
+    def set_database_key(self, idata):
+        self.database_key = idata
+
+    def get_email(self):
+        return self.email
+
+    def set_email(self, idata):
+        self.email = idata
