3829030b67c42ab7c9f5067dfcffb09672c343a4
2102596sit <2102596@sit.singaporetech.edu.sg>
2023-03-14 00:18:12 +0800

    removing populated reports and unused functions for integration
    removing populated reports and unused functions for integration

--- TerraformDemo/validate.tf
+++
@@ -1,7 +0,0 @@
-resource "null_resource" "validate_config" {
-  provisioner "local-exec" {
-    command = "python validate_config.py ${var.tf_config_file}"
-  }
-
-  depends_on = [terraform.workspace]
-}
