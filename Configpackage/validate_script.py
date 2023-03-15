713161fbc7ad811d9a7baa3b129593128c5aa32a
Kenn <kenneth.chng@hotmail.com>
2023-03-16 02:29:49 +0800

    validate_script changes
    Report Formatting and mysql check logic fix

--- Configpackage/validate_script.py
+++ Configpackage/validate_script.py
@@ -13,56 +13,73 @@ from reportlab.platypus import SimpleDocTemplate, Paragraph
 from bs4 import BeautifulSoup
 from datetime import datetime
 
+#Global variable for path
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-TPL_FOLDER = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))),'Terraform/scripts')
+TPL_FOLDER = os.path.join(PROJECT_ROOT,'Terraform/scripts')
 
 def mysqlSecureCheck(command,mysqlSecureCommands):
 	root_pass_pattern = r'mysql\s-e\s"UPDATE\smysql.user\sSET\sPassword(\s)?=(\s)?PASSWORD\(\'[\S]+.\)\sWHERE\sUser(\s)?=(\s)?\'root\''
 	drop_anonyUser_pattern =r'mysql -e "DROP\sUSER\s\'\'\@\'\$\([\S]+.\)\'"'
 	drop_test_table=r'mysql -e "DROP DATABASE test"'
 	flush_pattern=r'mysql -e "FLUSH PRIVILEGES"'
+
 	if re.search(root_pass_pattern,command) and len(mysqlSecureCommands) != 0:
 		for i in range(len(mysqlSecureCommands)):
 			if re.search(root_pass_pattern,mysqlSecureCommands[i]):
 				mysqlSecureCommands.pop(i)
 				break
+
 	if re.search(drop_test_table,command) and len(mysqlSecureCommands) != 0:
 		for i in range(len(mysqlSecureCommands)):
 			if re.search(drop_test_table,mysqlSecureCommands[i]):
 				mysqlSecureCommands.pop(i)
 				break
+
 	if re.search(drop_anonyUser_pattern,command) and len(mysqlSecureCommands) != 0:
 		for i in range(len(mysqlSecureCommands)):
 			if re.search(drop_anonyUser_pattern,mysqlSecureCommands[i]):
 				mysqlSecureCommands.pop(i)
 				break
+
 	if len(mysqlSecureCommands)==0:
 		match = re.search(flush_pattern,command)
 		if match:
 			return True
+
 	return False
 
 def extract_package_info(command):
 	pattern = r"install\s(.+)=([\d.]+)"
 	match = re.search(pattern, command)
 	if match:
-		package_name = match.group(1)
+		unparse_package_name = match.group(1)
+		parse_pattern = r"(.+)-(.+)"
+		parse_match = re.search(parse_pattern, unparse_package_name)
+		if parse_match:
+			package_name = parse_match.group(1)
+		else:
+			package_name = unparse_package_name
 		package_version = match.group(2)
 		return package_name, package_version
 	else:
 		return None
 
 def has_version_number(command):
-	pattern = r"=([\d.]+)(\s-y)?$"
+	pattern = r"apt(-get)? install\s(.+)(=([\d.]+)?)(\s-y)?$"
 	match = re.search(pattern, command)
 	return bool(match)
 
 def has_mql_installation(command):
-	pattern = r"apt(-get)? install\s(.+)(=([\d.]+)?)(\s-y)?"
+	db = ['mysql','mariadb']
+	pattern = r"apt(-get)? install\s(.+)=([\d.]+)?(\s-y)?"
 	match = re.search(pattern, command)
 	if match:
-		db = ['mysql','mariadb']
-		return match.group(2) in db	
+		unparse_package_name = match.group(2)
+		parse_pattern = r"(.+)-(.+)"
+		parse_match = re.search(parse_pattern, unparse_package_name)
+		if parse_match:
+			unparse_package_name = parse_match.group(1)
+		return unparse_package_name in db	
 	return False
 
 def getFileContent(filename):
@@ -71,16 +88,6 @@ def getFileContent(filename):
 		for line in file:
 			lines.append(line.strip())
 	return lines
-
-def checkAptUpdate(command):
-	pattern = r"apt(-get)? install\s(.+)(=([\d.]+)?)"
-	match = re.search(pattern, command)
-	if match:
-		package_name = match.group(1)
-		package_version = match.group(2)
-		return package_name, package_version
-	else:
-		return None
 
 def CVE_number(pkg_name, pkg_version):
 	link="https://www.cybersecurity-help.cz/vdb/list.php?search_line=Y&filter%5B%25SEARCH%5D={}+{}".format(pkg_name,pkg_version)
@@ -93,32 +100,34 @@ def CVE_number(pkg_name, pkg_version):
 		return None
 	else:
 		result_list = soup.find_all("div", {"class": "cvp_short"})
-		return result_list
+		return result_list, link
 
 
-def get_all_files():
-	lemp_files_dir = TPL_FOLDER # Specify directory with php file TODO: Get vuln php codes
+def get_all_lemp_files():
+	lemp_files_dir = TPL_FOLDER # Specify directory with php file
 	lemp_files_arr = []
 	get_tpl_files(lemp_files_dir, lemp_files_arr)
 	return lemp_files_arr
 
-# Function to get list of php files
+# Function to get list of tpl files
 def get_tpl_files(dir, result):
     # Loop through directory 
     for file in os.listdir(dir):
         # File uri
         file_path = os.path.join(dir, file)
-        # Check if isfile and with .php extension
+        # Check if isfile and with .tpl extension
         if os.path.isfile(file_path):
             if file.endswith('.tpl'):
                 result.append(file)
 
+# Functions to scan and generate tpl validation report
 def generate_script_report():
 	apt_update_checked = False
 	validated = False
 	mysqlSecureChecked = False
-
-	mysqlSecureCommands = ["sudo mysql -e \"UPDATE mysql.user SET Password = PASSWORD(\'$database_pwd\') WHERE User = \'root\'\"",
+	clickable = "Click here for more detail!"
+	mysqlSecureCommands = [	
+	"sudo mysql -e \"UPDATE mysql.user SET Password = PASSWORD(\'$database_pwd\') WHERE User = \'root\'\"",
 	'sudo mysql -e \"DROP USER \'\'@\'$(hostname)\'\"',
 	'sudo mysql -e "DROP DATABASE test"']
 
@@ -128,37 +137,47 @@ def generate_script_report():
 	lemp_report = SimpleDocTemplate("./report/2_report_scripts.pdf", pagesize=landscape(A1))
 	styles = getSampleStyleSheet()
 	report_info = []
-	lemp_files = get_all_files()
+	lemp_files = get_all_lemp_files()
+
+	title_style = styles["Title"]
+	title_style.fontSize = 40
+	title_style.leading = 40 * 2
+	title = f"<u>Validations on tpl files</u>"
+	title_data = Paragraph(title, title_style)
+	report_info.append(title_data)
 
 	for file in lemp_files:
-		package_flagged = {}
 		file_content_list = getFileContent(TPL_FOLDER+'/'+file)
-		section_style = styles["Heading1"]
-		section_style.alignment = 0
-		section = f"<u>Validations on file: {file}</u>"
-		section_data = Paragraph(section, section_style)
-		report_info.append(section_data)
-		report_info_len = len(report_info)
+		file_section_style = styles["Heading1"]
+		file_section_style.fontSize = 24
+		file_section_style.leading = 24 * 1.2
+		file_section_style.alignment = 0
+		file_section = f"<u>File:\t{file}</u>"
+		file_section_data = Paragraph(file_section, file_section_style)
+		report_info.append(file_section_data)
+
+		package_flagged = {}
 		sql_flag = False
+		report_info_len = len(report_info)
 		for i in file_content_list:
 			if not sql_flag:
 				sql_flag = has_mql_installation(i)
-			# Check Apt update
+			# Verify Apt update
 			if not validated:
+				# If Apt update not checked, check every line for installing command
 				if not apt_update_checked:
 					apt_install_match = re.search(apt_install_pattern,i)
+					# If found, prompt warning
 					if apt_install_match:
-						section_style = styles["Heading2"]
-						section_style.alignment = 0
-						section = "<u>[WARNING!] apt update is not performed before installtions of package</u>"
-						section_data = Paragraph(section, section_style)
-						report_info.append(section_data)
-						pkgname_style = styles["Heading3"]
-						pkgname_style.alignment = 0
-						pkgname_style.textColor = colors.red
-						pkgname = f"Please run the following command before any package installtions using apt!"
-						pkgname_data = Paragraph(pkgname, pkgname_style)
-						report_info.append(pkgname_data)
+						apt_style = styles["Heading3"]
+						apt_style.fontSize = 18
+						apt_style.leading = 18 * 1.2
+						apt_style.alignment = 0
+						apt_style.textColor = colors.red
+						apt = "WARNING! apt update is not perform before installtions of package"
+						apt_data = Paragraph(apt, apt_style)
+						report_info.append(apt_data)
+
 						formatLine = f"|:|<b>sudo apt update </b>\n<br/>{'-' * 180}"
 						line_style = styles["Code"]
 						line_style.fontSize = 14
@@ -166,48 +185,52 @@ def generate_script_report():
 						line_style.underline = 1
 						line_style.alignment = 0
 						line_data = Paragraph(formatLine, line_style)
-						report_info.append(line_data)	
-						# print("[!]************** Warning! apt update is not run before installtions from apt.")
+						report_info.append(line_data)
+
+						pkgname_style = styles["Normal"]
+						pkgname_style.alignment = 0
+						pkgname_style.fontSize = 12
+						pkgname_style.leading = 12 * 1.2
+						pkgname_style.textColor = colors.blue
+						pkgname = f"Before installing any packages using apt, it is advisable to run the above command.<br></br>"
+						pkgname_data = Paragraph(pkgname, pkgname_style)
+						report_info.append(pkgname_data)	
+
 						validated = True
+					# If not found, check apt update command
 					if not validated:
 						apt_update_match = re.search(apt_update_pattern,i)
 						if apt_update_match:
 							apt_update_check = True
 							validated = True
+
 			# Check specificed package vulnerabilities
 			if has_version_number(i):
 				package_name, package_version = extract_package_info(i)
-				query = CVE_number(package_name, package_version)
+				query , url= CVE_number(package_name, package_version)
 				if query:
 					package_details = []
-					# print("\n[!]*************Package: "+package_name+"\tVersion: "+package_version+" Vulnerabilities found!\n")
 					for x in query:
 						package_details.append(x.get_text(" ", strip=True))
+					package_details.append(url)
 					package_flagged[package_name+' '+package_version] = package_details
-				else:
-					print("No vulnerabilities found for "+package_name+":"+package_version)
+
+			# If particular file has install mysql or mariadb thn validate secure installation
 			if sql_flag:
 				if not mysqlSecureChecked:
 					mysqlSecureChecked=mysqlSecureCheck(i,mysqlSecureCommands)
-
+		# If result found for package x, print findings
 		if len(package_flagged)>1:
-			section_style = styles["Heading2"]
-			section_style.alignment = 0
-			section = f"<u>Vulnerable Packages found: </u>"
-			section_data = Paragraph(section, section_style)
-			report_info.append(section_data)
-
 			for key, value in package_flagged.items():
-				# print("*"*50)
-				# print(f"Potential vulnerability package found in: [{key}]")
-
 				pkgname_style = styles["Heading3"]
+				pkgname_style.fontSize = 18
+				pkgname_style.leading = 18 * 1.2
 				pkgname_style.alignment = 0
 				pkgname_style.textColor = colors.red
 				pkgname = f"Potential vulnerability found in: [{key}]"
 				pkgname_data = Paragraph(pkgname, pkgname_style)
 				report_info.append(pkgname_data)
-
+				pkg_link = value.pop()
 				for line in value:
 					formatLine = f"|:|<b>{line}</b>\n<br/>{'-' * 180}"
 					line_style = styles["Code"]
@@ -217,6 +240,15 @@ def generate_script_report():
 					line_style.alignment = 0
 					line_data = Paragraph(formatLine, line_style)
 					report_info.append(line_data)
+
+				link_style = styles["Normal"]
+				link_style.fontSize = 12
+				link_style.leading = 12 * 1.2
+				link_style.textColor = colors.blue
+				link_style.alignment = 0
+				link_data = Paragraph('<u><link href="' + pkg_link + '">' + clickable + '</link></u><br></br>', link_style)
+				report_info.append(link_data)
+		# If report_info_len didnt change, conclude no vul found!
 		if len(report_info)==report_info_len:
 			pkgname_style = styles["Heading2"]
 			pkgname_style.alignment = 0
@@ -224,26 +256,20 @@ def generate_script_report():
 			pkgname = f"No vulnerability found in: [{file}]"
 			pkgname_data = Paragraph(pkgname, pkgname_style)
 			report_info.append(pkgname_data)
+		# If file flagged mysql/mariadb installed
 		if sql_flag:
 			# Display check result of mysql_secure_installation
 			if mysqlSecureChecked is False:
-				section_style = styles["Heading2"]
-				section_style.alignment = 0
-				section_style.textColor = colors.black
-				section = f"<u>Mysql-secure-installation is not performed properly on file: {file}</u>"
-				section_data = Paragraph(section, section_style)
-				report_info.append(section_data)
+				mysql_style = styles["Heading3"]
+				mysql_style.fontSize = 18
+				mysql_style.leading = 18 * 1.2
+				mysql_style.alignment = 0
+				mysql_style.textColor = colors.red
+				mysql = f"Mysql-secure-installation is not performed properly! The following command was not executed."
+				mysql_data = Paragraph(mysql, mysql_style)
+				report_info.append(mysql_data)
 
 				if len(mysqlSecureCommands)>0:
-					# print("[!] ************************************* Following command Not performed!")
-					# print("\n["+" || 	".join(mysqlSecureCommands)+"]")
-
-					pkgname_style = styles["Heading3"]
-					pkgname_style.alignment = 0
-					pkgname_style.textColor = colors.red
-					pkgname = f"Following command Not performed!"
-					pkgname_data = Paragraph(pkgname, pkgname_style)
-					report_info.append(pkgname_data)
 					for xx in mysqlSecureCommands:
 						formatLine = f"|:|<b>{xx}</b>\n<br/>{'-' * 180}"
 						line_style = styles["Code"]
@@ -254,13 +280,6 @@ def generate_script_report():
 						line_data = Paragraph(formatLine, line_style)
 						report_info.append(line_data)
 				else:
-					# print("[!] ************************************* \"FLUSH PRIVILEGES\" Not performed!")
-					pkgname_style = styles["Heading3"]
-					pkgname_style.alignment = 0
-					pkgname_style.textColor = colors.red
-					pkgname = f"FLUSH PRIVILEGES command Not performed properly!"
-					pkgname_data = Paragraph(pkgname, pkgname_style)
-					report_info.append(pkgname_data)
 					formatLine = f"|:|<b>sudo mysql -e \"FLUSH PRIVILEGES\"</b>\n<br/>{'-' * 180}"
 					line_style = styles["Code"]
 					line_style.fontSize = 14
@@ -269,5 +288,14 @@ def generate_script_report():
 					line_style.alignment = 0
 					line_data = Paragraph(formatLine, line_style)
 					report_info.append(line_data)
-	
+
+				sqlReco_style = styles["Normal"]
+				sqlReco_link = "https://bertvv.github.io/notes-to-self/2015/11/16/automating-mysql_secure_installation/"
+				sqlReco_style.alignment = 0
+				sqlReco_style.fontSize = 12
+				sqlReco_style.leading = 12 * 1.2
+				sqlReco_style.textColor = colors.blue
+				sqlReco_data = Paragraph('<u><link href="' + sqlReco_link + '">' + clickable + '</link></u><br></br>', sqlReco_style)
+				report_info.append(sqlReco_data)
+	# build report
 	lemp_report.build(report_info)
---
+++ Terraform/scripts/very_vuln.tpl
@@ -0,0 +1,137 @@
+#!/bin/bash
+timestamp=`date "+%Y-%m-%d %H:%M:%S"`
+log="/home/adminuser/lempstack-install.log"
+touch /home/adminuser/lempstack-install.log
+
+domain="INSERTYOURDOMAINHERE"
+
+#===================================================================#
+# Create new user
+#===================================================================#
+sudo useradd webadm
+sudo usermod -aG sudo webadm
+echo "$timestamp Created webadm user" >> $log
+#===================================================================#
+# Configure firewall #TODO
+#===================================================================#
+# sudo ufw limit ssh
+# sudo ufw enable
+
+# # Write configs to sshd_config
+# sudo echo "
+# PermitRootLogin no
+# PasswordAuthentication no
+# AllowUsers adminuser" >> /etc/ssh/sshd_config 
+
+# # Configure SSH to run on port 1002
+# sudo ufw limit 1002/tcp
+# sudo echo "Port 1002" >> /etc/ssh/sshd_config
+
+# sudo systemctl restart ssh
+
+#===================================================================#
+# Install LEMP Stack
+#===================================================================#
+# Install Nginx
+sudo apt install nginx-extras=1.17.0 -y
+sudo ufw allow 'Nginx Full'
+echo "$timestamp Installed Nginx" >> $log
+
+# Install MariaDB
+sudo apt install mariadb-server=10.3.34 -y
+
+# # mysql_secure_installation #TODO
+# # Setting the root password
+# sudo mysql -e "UPDATE mysql.user SET Password = PASSWORD('xia0Ming') WHERE User = 'root'"
+# # Remove anonymous users
+# # Make changes take effect
+# sudo mysql -e "FLUSH PRIVILEGES"
+
+echo "$timestamp Installed MariaDB" >> $log
+
+# Install PHP
+sudo apt install php-fpm=7.1.12 -y
+sudo apt install samba=3.6.3 -y
+sudo apt install vsftpd=2.3.4 -y
+sudo apt install php-mysql=7.1.0 -y
+sudo cp -a -v /etc/nginx/sites-available/default /etc/nginx/sites-available/$domain
+sudo chmod 666 /etc/nginx/sites-available/$domain
+sudo echo "
+server {
+        listen 80;
+        listen [::]:80;
+
+        root /var/www/html;
+
+        # Add index.php to the list if you are using PHP
+        index index.php index.html index.htm index.nginx-debian.html;
+
+        server_name $domain;
+
+        location / {
+                # First attempt to serve request as file, then
+                # as directory, then fall back to displaying a 404.
+                try_files \$uri \$uri/ =404;
+        }
+
+        # pass PHP scripts to FastCGI server
+        #
+        location ~ \.php$ {
+                include snippets/fastcgi-php.conf;
+
+                # With php-fpm (or other unix sockets):
+                fastcgi_pass unix:/var/run/php/php7.4-fpm.sock;
+        #       # With php-cgi (or other tcp sockets):
+        #       fastcgi_pass 127.0.0.1:9000;
+        }
+
+        # deny access to .htaccess files, if Apache's document root
+        # concurs with nginx's one
+        #
+        location ~ /\.ht {
+                deny all;
+        }
+}" > /etc/nginx/sites-available/$domain
+sudo chmod 644 /etc/nginx/sites-available/$domain
+sudo ln -s /etc/nginx/sites-available/$domain /etc/nginx/sites-enabled/
+sudo unlink /etc/nginx/sites-enabled/default
+sudo systemctl restart nginx
+
+sudo chown -R adminuser:adminuser /var/www/html
+sudo chmod 2775 /var/www/html
+
+sudo echo "
+<!DOCTYPE html>
+<html>
+        <body style='background-color:red'>
+                <h1 style='text-align:center'> ICT2206 CLOUD-INIT AUTOMATION TEST <h1>
+
+                <?php echo \"<h3 style='text-align:center'> PHP interpreter successfully installed </h3>\"; ?>
+        </body>
+</html>" > /var/www/html/index.php
+
+echo "$timestamp Installed PHP" >> $log
+
+#===================================================================#
+# Setting up HTTPS using let's encrypt cert
+# Commented out to prevent over usage of let's encrypt cert, uncomment to test
+#===================================================================#
+# sudo snap install core
+# sudo snap refresh core
+# sudo snap install --classic certbot
+
+# sudo certbot --nginx -d $domain --email $email --agree-tos
+# sudo systemctl restart nginx
+
+#===================================================================#
+# Enable HTTP/2 
+# Commented out to prevent over usage of let's encrypt cert, uncomment to test
+#===================================================================#
+# sudo sed -i 's/443 ssl ipv6only=on;/443 ssl http2 ipv6only=on;/' /etc/nginx/sites-available/$domain
+# sudo sed -i 's/listen 443 ssl;/listen 443 ssl http2;/' /etc/nginx/sites-available/$domain
+
+#===================================================================#
+# Enable HSTS
+# Commented out to prevent over usage of let's encrypt cert, uncomment to test
+#===================================================================#
+# sudo sed -i '/^http {/a \       add_header Strict-Transport-Security "max-age=15768000; includeSubDomains" always;' /etc/nginx/nginx.conf
